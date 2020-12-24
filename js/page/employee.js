76d5e2e6fb4db50086ca22c4dd6ec45fe7afab45
pqkhanh <pqkhanh@FRESHER-74>
2020-12-24 11:03:19 +0700

    js jquery how to use

---
+++ js/page/employee.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    loadData(1);
+});
+
+function loadData() {
+    //Lấy dữ liệu về:
+    $.ajax({
+        url: "https://api.pqkhanh.net/api/employees",
+        method: "GET",
+    }).done(function (res) {
+        var data = res;
+    }).fail(function (res) {
+
+    })
+    //binding dữ liệu lên table:
+}
