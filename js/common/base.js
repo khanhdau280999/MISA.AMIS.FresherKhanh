019b8e8f8b7e0ee740d3274043bd2be79a45a2ee
pqkhanh <pqkhanh@FRESHER-74>
2020-12-25 15:13:14 +0700

    baseJS add fieldName to html

---
+++ js/common/base.js
@@ -0,0 +1,42 @@
+﻿class BaseJS {
+    constructor() {
+        this.getDataUrl = null;
+        this.setDataUrl();
+        this.loadData();
+    }
+    setDataUrl() {
+
+    }
+
+    loadData() {
+
+        // Lấy thông tin các cột dữ liệu
+        var columns = $('table thead th');
+        var fieldNames = [];
+        var getDataUrl = this.getDataUrl;
+
+        //Lấy dữ liệu về:
+        $.ajax({
+            url: getDataUrl,
+            method: "GET",
+        }).done(function (res) {
+            $.each(res, function (index, obj) {
+                var tr = $(`<tr></tr>`);
+                // Lấy thông tin dữ liệu sẽ map tương ứng với các cột:
+                $.each(columns, function (index, th) {
+                    var td = $(`<td><div><span></span></div></td>`);
+                    var fieldName = $(th).attr('fieldname');
+                    var value = obj[fieldName];
+                    td.append(value)
+                    $(tr).append(td);
+                })
+                $('table tbody').append(tr);
+            })
+
+        }).fail(function (res) {
+
+        })
+
+    }
+
+}
--- js/page/customer.js
+++ js/page/customer.js
@@ -1,72 +1,97 @@
-﻿$(document).ready(function () {
-    loadData(1);
+﻿
+$(document).ready(function () {
+    new CustomerJS();
 });
 
+
+class CustomerJS extends BaseJS {
+    constructor() { 
+        super();
+    }
+
+    setDataUrl() {
+        this.getDataUrl = "http://api.manhnv.net/api/customers";
+    }
 /**
  * Load dữ liệu
  * CreatedBy: NVKhanh (25/12/2020)
  * */
-function loadData() {
-    //Lấy dữ liệu về:
-    $.ajax({
-        url: "http://api.manhnv.net/api/employees",
-        method: "GET",
-    }).done(function (res) {
-        var data = res;
-        $.each(data, function (index, item) {
-            var dateOfBirth = item["DateOfBirth"];
-            var salary = item.Salary;
-            salary = formatMoney(salary);
-            dateOfBirth = formatDate(dateOfBirth);
-            var checkbox = `<input type="checkbox" />`;
-            if (item.Gender > 0) {
-                var checkbox = `<input type="checkbox" checked />`;
-            }
-            var tr = $(`<tr>
-                        <td><div><span>`+ item.EmployeeCode + `</span></div></td>
-                        <td><div><span>`+ item['FullName'] + `</span></div></td>
-                        <td><div class="text-align-center">`+ checkbox + `</div></td>
-                        <td><div><span>`+ dateOfBirth + `</span></div></td>
-                        <td><div><span>`+ item['PhoneNumber'] + `</span></div></td>
-                        <td><div><span>`+ item['Email'] + `</span></div></td>
-                        <td><div style="max-width:250px"><span>`+ item['Address'] + `</span></div></td>
-                    </tr>`);
-            $('table tbody').append(tr);
-        });
+    //loadData() {
+    //    //Lấy dữ liệu về:
+    //    $.ajax({
+    //        url: "http://api.manhnv.net/api/customers",
+    //        method: "GET",
+    //    }).done(function (res) {
+    //        var data = res;
+    //        $.each(data, function (index, item) {
+    //            var dateOfBirth = item["DateOfBirth"];
+    //            //var amount = item.DebitAmount;
+    //            //amount = formatMoney(amount);
+    //            dateOfBirth = formatDate(dateOfBirth);
+    //            var checkbox = `<input type="checkbox" />`;
+    //            if (item.Gender > 0) {
+    //                var checkbox = `<input type="checkbox" checked />`;
+    //            }
+    //            var tr = $(`<tr>
+    //                    <td><div><span>`+ item.CustomerCode + `</span></div></td>
+    //                    <td><div><span>`+ item['FullName'] + `</span></div></td>
+    //                    <td><div class="text-align-center">`+ checkbox + `</div></td>
+    //                    <td><div><span>`+ dateOfBirth + `</span></div></td>
+    //                    <td><div><span>`+ item['CustomerGroupName'] + `</span></div></td>
+    //                    <td><div><span>`+ item['PhoneNumber'] + `</span></div></td>
+    //                    <td><div><span>`+ item['Email'] + `</span></div></td>
+    //                    <td><div style="max-width:250px"><span>`+ item['CompanyName'] + `</span></div></td>
+    //                    <td><div><span>`+ item['DebitAmount'] + `</span></div></td>
+    //                    <td><div><span>`+ item['CompanyTaxCode'] + `</span></div></td>
+    //                </tr>`);
+    //            $('table tbody').append(tr);
+    //        });
 
-    }).fail(function (res) {
+    //    }).fail(function (res) {
 
-    })
-    //binding dữ liệu lên table:
+    //    })
+    ////binding dữ liệu lên table:
+    //}
+
+   
 }
 
-/**
- * CreatedBy: NVKHANH (25/12/2020)
- * Format dữ liệu ngày tháng sang ngày/tháng/năm
- * @param {any} date tham số có kiểu dữ liệu bất kỳ
- * 
- */
-function formatDate(date) {
-    var date = new Date(date);
-    if (Number.isNaN(date.getTime())) {
-        return "";
-    } else {
-        var day = date.getDate(),
-            month = date.getMonth() + 1,
-            year = date.getFullYear();
-        day = day < 10 ? '0' + day : day;
-        month = month < 10 ? '0' + month : month;
-        return day + '/' + month + '/' + year;
-    }
 
-}
+///**
+// * Load dữ liệu
+// * CreatedBy: NVKhanh (25/12/2020)
+// * */
+//function loadData() {
+   
+//}
 
-/**
- * Hàm định dạng hiển thị tiền tệ
- * @param {Number} money Số tiền
- * CreatedBy: NVKHANH (25/12/2020)
- */
-function formatMoney(money) {
-    var num = money.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-    return num;
-}
+///**
+// * CreatedBy: NVKHANH (25/12/2020)
+// * Format dữ liệu ngày tháng sang ngày/tháng/năm
+// * @param {any} date tham số có kiểu dữ liệu bất kỳ
+// * 
+// */
+//function formatDate(date) {
+//    var date = new Date(date);
+//    if (Number.isNaN(date.getTime())) {
+//        return "";
+//    } else {
+//        var day = date.getDate(),
+//            month = date.getMonth() + 1,
+//            year = date.getFullYear();
+//        day = day < 10 ? '0' + day : day;
+//        month = month < 10 ? '0' + month : month;
+//        return day + '/' + month + '/' + year;
+//    }
+
+//}
+
+///**
+// * Hàm định dạng hiển thị tiền tệ
+// * @param {Number} money Số tiền
+// * CreatedBy: NVKHANH (25/12/2020)
+// */
+//function formatMoney(money) {
+//    var num = money.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+//    return num;
+//}
